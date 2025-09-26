758e476c0ba37241dbc9475a580541d51ec094d4
Pushkin4000 <pushkinranjan@gmail.com>
2025-09-26 17:39:37 +0530

    Switched to groq_api approach
    For folks who don't have ollama.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import ollama_helpz
+import langchain_helpz
 import time
 
 st.title("Plan Craft AI")
@@ -9,7 +9,7 @@ Technology=st.text_input("Enter the name of technology for project ideas and pro
 
 if Technology:
     with st.spinner(text="Please wait uwu!!!!!!"):
-        response=ollama_helpz.techo(Technology)
+        response=langchain_helpz.techo(Technology)
     st.header(response["project_name"])
     zap=response["Procedure"].split(",")
     for items in zap:
